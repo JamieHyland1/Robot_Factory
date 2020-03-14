c5b733d36e719ead01bd28c669834e7b88af8b47
Jamie <Jamiehyland01@gmail.com>
2020-03-14 14:32:42 +0000

    Added ArrayList for the robots, should be updated to thread pool

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
+import java.util.ArrayList;
+
 public class Main{
     public static void main(String[] args){
+        ArrayList<Robot> robots = new ArrayList<Robot>();
         System.out.println("Hello world");
 
     }
