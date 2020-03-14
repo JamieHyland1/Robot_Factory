71936bddc2fd08e970b65ff802293acbff82f3f0
Jamie <Jamiehyland01@gmail.com>
2020-03-14 15:22:09 +0000

    Added basic swing support to main file

--- Main.java
+++ Main.java
@@ -1,9 +1,15 @@
 import java.util.ArrayList;
+import javax.swing.*;  
 
 public class Main{
     public static void main(String[] args){
+        JFrame f = new JFrame();
         ArrayList<Robot> robots = new ArrayList<Robot>();
         System.out.println("Hello world");
 
+        
+        f.setSize(500,500);
+        f.setLayout(null);
+        f.setVisible(true);
     }
 }
