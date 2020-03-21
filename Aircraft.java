35042be4484affacf7f697254e3c595cd4f0f69d
Jamie <Jamiehyland01@gmail.com>
2020-03-21 15:48:25 +0000

    Switched to int instead of arraylist for testing

--- Aircraft.java
+++ Aircraft.java
@@ -3,9 +3,10 @@ import java.util.ArrayList;
 public class Aircraft{
 
     private String id;
-    private ArrayList<Integer> parts_needed = new ArrayList<Integer>();
+    //private ArrayList<Integer> parts_needed = new ArrayList<Integer>();
+    private int parts_needed;
 
-    public Aircraft(String id, ArrayList<Integer> parts_needed){
+    public Aircraft(String id, int parts_needed){
         //default constructor
         this.id = id;
         this.parts_needed = parts_needed;
@@ -15,7 +16,19 @@ public class Aircraft{
         return id;
     }
 
-    public ArrayList<Integer> getPartsNeeded() {
+    public Boolean installParts(){
+        if(this.parts_needed > 0) 
+        {
+            this.parts_needed--;
+            return true;
+        }
+        else {
+            System.out.println("no parts needed");
+        }
+        return false;
+    }
+
+    public int getPartsNeeded() {
         return parts_needed;
     }
 
