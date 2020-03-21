4cafac134e8087ccc44362627c6c9a626f727c53
Jamie <Jamiehyland01@gmail.com>
2020-03-21 15:47:25 +0000

    Implemented main thread for robot

--- Robot.java
+++ Robot.java
@@ -2,14 +2,26 @@ public class Robot extends Thread {
 
     private String id;
     private int installAmount;
-    private Factory factory;
-    private Aircraft workingAircraft;
+    // private Factory factory;
+    public Aircraft workingAircraft;
+    private int aircraftsInWaiting;
+    private boolean active = false;
 
-    public Robot(Factory factory, final String id) {
-        this.factory = factory;
+    public Robot(String id) {
         this.id = id;
-        this.installAmount = (int) Math.floor(Math.random() * (5) + 1);
         this.workingAircraft = null;
+    }
+
+    // public Robot( final String id) {
+    // // this.factory = factory;
+    // this.id = id;
+    // this.installAmount = (int) Math.floor(Math.random() * (5) + 1);
+    // this.workingAircraft = null;
+    // }
+
+    public void StartOperating() {
+        this.active = true;
+        this.start();
     }
 
     public String getID() {
@@ -20,48 +32,67 @@ public class Robot extends Thread {
         this.id = id;
     }
 
-    public void start() {
-        System.out.println("Starting Robot " + this.getID());
-    }
-
-    public synchronized void setWorkingAircraft(Aircraft aircraft) {
+    // assign a robot an aircraft
+    public synchronized void setWorkingAircraft(Aircraft aircraft, int aircraftsInWaiting) {
+        this.aircraftsInWaiting = aircraftsInWaiting;
         this.workingAircraft = aircraft;
+        notify(); //if the thread is asleep notify it that it was given an aircraft
     }
 
+    // supply the operator with an aircraft
     public Aircraft getWorkingAircraft() {
-        return this.workingAircraft;
+        Aircraft a = this.workingAircraft;
+        this.workingAircraft = null;
+        return a;
     }
 
-    // main logic for robot goes in here
-    // not sure if this is correct? I dont think we can supply the parts variable at runtime as java is pass by value
-    
-    public void run(Aircraft a, int parts) {
-        System.out.println("Running current Robot...");
-        System.out.println("*robot noises*");
-        synchronized (this) {
-            parts -= this.installAmount;
-            System.out.println("Robot " + this.id + " Installing " + this.installAmount + " parts on aircraft");
-            long timeToInstall = 100 * this.installAmount; //time taken to install is based off how many parts are needed 
-            try {
-                this.sleep(timeToInstall);
-            } catch (InterruptedException e) {
-                System.out.println("Robot " + this.id + " was unable to install parts");
-                e.printStackTrace();
+    // finish the working thread.
+    public void FinishOperations() {
+        System.out.println("Robot " + this.getID() + " shutting down...");
+        this.active = false;
+    }
+
+    // this.active just makes sure the robot runs for the entirety of the program
+    // still need to add code to get more parts
+    public void run() throws NullPointerException {
+        System.out.println("Starting up");
+        while (this.active) {
+            if (this.workingAircraft == null && this.aircraftsInWaiting > 0) {
+                System.out.println(this.getID() + ": No aircraft supplied, going to sleep...zZzZzZz");
+                synchronized (this) {
+                    try {
+                        this.wait();
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            } else if(this.aircraftsInWaiting > 0) {
+                System.out.println("Waking up!");
+              //  this.getWorkingAircraft();
+                synchronized (this.workingAircraft) {
+                    System.out.println("Robot " + this.getID() + " installing parts on aircraft " + this.workingAircraft.getId());
+                    System.out.println(this.workingAircraft.getPartsNeeded() + " parts left to install");
+                    while(this.workingAircraft.getPartsNeeded() > 0){
+                        try {
+                            this.workingAircraft.installParts();
+                            System.out.println("*clank clank*");
+                            System.out.println(this. workingAircraft.getPartsNeeded() + " parts needed left to install on " + this.workingAircraft.getId()); 
+                            this.sleep(500);
+                        } catch (InterruptedException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.println(this.getID() + " finished working on " + this.workingAircraft.getId());
+                    this.getWorkingAircraft();
+                }              
+            }else{
+                System.out.println("No aircrafts left");
+                this.FinishOperations();
             }
+           
         }
     }
-
-    public void waiting() {
-        System.out.println("Robot " + this.getID() + " is waiting for another Aircraft");
-        // wait for 1 second
-        try {
-            this.sleep(1000);
-        } catch (final InterruptedException e) {
-           System.out.println(e);
-       }
-    }
-
-
-    //Add method to notify system its done installing parts on aircraft
-    
 }
+
