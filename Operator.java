5c279dd2df51081949db70fe7d08abbda961cf74
Jamie <Jamiehyland01@gmail.com>
2020-03-22 12:19:55 +0000

    finished operator

--- Operator.java
+++ Operator.java
@@ -1,119 +1,132 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Queue;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 import java.util.ArrayDeque;
 
-public class Operator {
-    
+public class Operator extends Thread {
+
     private Factory factory;
     private int partsPool;
     private ArrayList<Robot> robots;
     private Queue<Robot> robotsWaitingList;
+    private HashSet<Integer> robotsInWaiting;
+    private boolean operating = true;
 
-    public Operator(Factory factory){
+    public Operator(Factory factory) {
         this.factory = factory;
         this.partsPool = 5;
         this.robots = factory.getRobots();
         this.robotsWaitingList = new ArrayDeque<Robot>();
+        this.robotsInWaiting = new HashSet<Integer>();
+        for(int i = 0; i < 10; i++){
+            robotsInWaiting.add(i);
+        }
     }
-    
+
     public int getPartsCount() {
         return this.partsPool;
     }
 
-    public void takeParts(Robot robot) {
-        factory.execute(() -> {
-            synchronized (this) {
-                int partsNeeded = robot.getInstallAmount();
-                if (this.partsPool >= partsNeeded) {
-                    this.partsPool -= partsNeeded;
-                    System.out.println("Parts given to Robot " + robot.getID() + ". There now are " + this.partsPool + " parts remaining.");
-                }
-                else {
-                    if (!robotsWaitingList.contains(robot)) {
-                        System.out.println("Not enough parts avaiable for " + robot + ". Waiting for parts...");
-                        robotsWaitingList.add(robot);
-                        waitingArea(this.partsPool);
-                    }
-                }
-            }
-        });
+    public synchronized boolean getAircraftsQueue(){
+        return this.factory.getAirCrafts().isEmpty();
     }
 
-    public void orderParts(int order) {
-        
-        // factory.execute(() -> {
-        //     synchronized (this) {
-                if (order > 0) {
-                    System.out.println("\nNew parts ordered. They will be delivered shortly...");
-                    try {
-                        Thread.sleep(3000);
-                    } 
-                    catch (final InterruptedException e) {
-                        System.out.println(e);
-                    }
-                    this.partsPool = this.partsPool + order;
-                    System.out.println("Parts have been restocked. There are now " + getPartsCount() + " parts in stock.\n");
+    public void enterWaiting(int id){
+        if(!this.robotsInWaiting.contains(id)){
+            System.out.println("Removing robot" + id + " from waiting list");
+            this.robotsInWaiting.add(id);
+            System.out.println("Current waiting List: " + this.robotsInWaiting.toString());
+        }
+    }
+
+    public  void leaveWaiting(int id){
+        System.out.println("Removing robot" + id + " from waiting list");
+        this.robotsInWaiting.remove(id);
+        System.out.println("Current waiting List: " + this.robotsInWaiting.toString());
+    }
+
+    public synchronized boolean checkProduction() {
+        System.out.println(this.robotsInWaiting.toString());
+        System.out.println(this.factory.getAirCrafts().isEmpty());
+        return (10 == this.robotsInWaiting.size() && this.factory.getAirCrafts().isEmpty());
+    }
+
+    public void takeParts(Robot robot) {
+        synchronized (this) {
+            int partsNeeded = robot.getInstallAmount();
+            if (this.partsPool >= partsNeeded) {
+                this.partsPool -= partsNeeded;
+                System.out.println("Parts given to Robot " + robot.getID() + ". There now are " + this.partsPool + " parts remaining.");
+            } else {
+                if (!robotsWaitingList.contains(robot)) {
+                    System.out.println("Not enough parts avaiable for " + robot + ". Waiting for parts...");
+                    robotsWaitingList.add(robot);
+                    waitingArea(this.partsPool);
                 }
-        //     }
-        // });
+            }
+        }
+    }
+
+    public synchronized Aircraft getAircraft(int id) {
+        Aircraft a = this.factory.getAirCrafts().poll();
+        if(a.getPartsNeeded().contains(id)){
+            this.leaveWaiting(id); // remove the robot from the waitingList
+            return a; //only give a robot an aircraft if it has the desired parts
+        }
+        this.factory.getAirCrafts().add(a);
+        return null;
     }
 
     public void moveAircraft(Aircraft aircraft) {
-        factory.execute(() -> {
-            synchronized (this) {
-                ArrayList<Integer> parts = aircraft.getPartsNeeded();
-                System.out.println(parts.toString());
-                if (parts.isEmpty()) {
-                    System.out.println("Aircraft is finished assembly. Removing from factory.");
-                    return;
-                }
-                for (int i = 0; i < parts.size(); i++) {
-                    Robot robot = (robots).get(parts.get(i));
-                    if (robot.getWorkingAircraft() == null) {
-                        System.out.println("Moving Aircraft " + aircraft.getID() + " to Robot " + robot.getID());
-                        try {
-                            Thread.sleep(5000); // wait for 100 ticks / 5 seconds
-                        } 
-                        catch (final InterruptedException e) {
-                            System.out.println(e);
-                        }
-                        robot.setWorkingAircraft(aircraft);
-                        System.out.println(robot + " is now working on Aircraft: " + aircraft.getID() + "\n");
-                        robot.getParts(robot);
-                        return;
-                    }
-                    else {
-                        System.out.println("Robot " + parts.get(i) + " is busy. Assigning to next Robot.");
-                    }
-                }
-                try {
-                    System.out.println("All Robots are working. Aircraft " + aircraft.getID() + " will have to wait.");
-                    Thread.sleep(5000);
-                    System.out.println("Tring to assign aircraft " + aircraft.getID() + " to a Robot again.");
-                    moveAircraft(aircraft);
-                } 
-                catch (final InterruptedException e) {
-                    System.out.println(e);
-                }
+        synchronized (this) {
+            ArrayList<Integer> parts = aircraft.getPartsNeeded();
+            System.out.println(aircraft.getID() + ": " + parts.toString());
+            if (parts.isEmpty()) {
+                System.out.println("Aircraft is finished assembly. Removing from factory.");
+                System.out.println("Robots are finished working: " + this.checkProduction());
+                return;
             }
-        });
+            System.out.println(this.factory.getAirCrafts().size());
+            this.factory.getAirCrafts().add(aircraft);
+            System.out.println("Robots are finished working: " + this.checkProduction());
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+               
+        }
+        
+    }
+    public void orderParts(int order) {
+        if (order > 0) {
+            System.out.println("\nNew parts ordered. They will be delivered shortly...");
+            try {
+                Thread.sleep(1500);
+            } 
+            catch (final InterruptedException e) {
+                System.out.println(e);
+            }
+            this.partsPool = this.partsPool + order;
+            System.out.println("Parts have been restocked. There are now " + getPartsCount() + " parts in stock.\n");
+        }
     }
 
-
     public  void waitingArea(int parts) {
-        factory.execute(() -> {
-            synchronized (this) {
-                int partsNow = parts;
-                if (partsPool <= 10){
-                    orderParts(50);
-                }
-                while (!robotsWaitingList.isEmpty()) {
-                    if (this.partsPool != partsNow) {
-                        takeParts(robotsWaitingList.poll());
-                    }
+        synchronized (this) {
+            int partsNow = parts;
+            if (partsPool <= 10){
+                orderParts(50);
+            }
+            while (!robotsWaitingList.isEmpty()) {
+                if (this.partsPool != partsNow) {
+                    takeParts(robotsWaitingList.poll());
                 }
             }
-        });
+        }
     }
 
 
